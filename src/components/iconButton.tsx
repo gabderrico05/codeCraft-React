2abda1a70ed3ac721d2fcf3d9e7ddeb75139de5a
Gabriel Derrico <derrico.g@aluno.ifsp.edu.br>
2025-02-22 21:16:52 -0300

    Criando a primeira tela do site e os componentes que irão ser usados

--- src/components/iconButton.tsx
+++ src/components/iconButton.tsx
@@ -1,14 +1,16 @@
-import { ReactNode } from "react"
+import type { ReactNode } from 'react'
 
 interface IconButtonProps {
   children: ReactNode
 }
 
-
-export function IconButton(props: IconButtonProps)  {
+export function IconButton(props: IconButtonProps) {
   return (
-  <button className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900">
-    {props.children}
+    <button
+      type="button"
+      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+    >
+      {props.children}
     </button>
   )
 }
